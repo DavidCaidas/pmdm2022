6cffb5ef2786d285ced8070fe232877ef391bdc7
David <104197044+DavidCaidas@users.noreply.github.com>
2022-09-27 16:50:59 +0200

    actualizacion y practica de vistas grupales horizontales y verticales

---
+++ app/src/main/java/com/davidcai/pmdm/ut01/ex01/presentation/Ut01Ex01Activity.kt
@@ -0,0 +1,13 @@
+package com.davidcai.pmdm.ut01.ex01.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.davidcai.pmdm.R
+
+class Ut01Ex01Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex01)
+    }
+
+}
