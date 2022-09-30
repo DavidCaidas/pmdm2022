ab40609f4d8fad5fa9fa816f80ccb28a057d72fd
David <104197044+DavidCaidas@users.noreply.github.com>
2022-09-30 20:14:07 +0200

    vistas

--- app/src/main/java/com/davidcai/pmdm/ut01/ex02/presentation/Ut01Ex02Activity.kt
+++ app/src/main/java/com/davidcai/pmdm/ut01/ex02/presentation/Ut01Ex02Activity.kt
@@ -1,12 +1,12 @@
-package com.davidcai.pmdm
+package com.davidcai.pmdm.ut01.ex02.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.davidcai.pmdm.R
 
-class MainActivity : AppCompatActivity() {
+class Ut01Ex02Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ut01ex02b)
-
+        setContentView(R.layout.activity_ut01ex02)
     }
 }
