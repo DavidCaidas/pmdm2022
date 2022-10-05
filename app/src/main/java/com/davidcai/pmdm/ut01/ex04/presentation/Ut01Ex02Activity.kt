5e919805c11f2988e07ac5782bd404a2393fc419
David <104197044+DavidCaidas@users.noreply.github.com>
2022-10-05 16:27:55 +0200

    ut01ex04 copia de interfaz to good to go

---
+++ app/src/main/java/com/davidcai/pmdm/ut01/ex04/presentation/Ut01Ex02Activity.kt
@@ -0,0 +1,12 @@
+package com.davidcai.pmdm.ut01.ex04.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.davidcai.pmdm.R
+
+class Ut01Ex02Activity : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex04)
+    }
+}
